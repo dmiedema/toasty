f43f6e780c786c27c9a1d968629ff6415931550b
dmiedema <daniel@househappy.org>
2014-11-18 10:10:34 -0800

    Add method outlets for the buttons

--- toasty/ViewController.m
+++ toasty/ViewController.m
@@ -7,21 +7,69 @@
 //
 
 #import "ViewController.h"
+#import <CRToast/CRToast.h>
 
 @interface ViewController ()
 
 @end
 
-@implementation ViewController
-
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+CRToastInteractionResponder * DMSwipeResponder() {
+    return [CRToastInteractionResponder interactionResponderWithInteractionType:CRToastInteractionTypeSwipe automaticallyDismiss:YES block:^(CRToastInteractionType interactionType) {
+        NSLog(@"Notifcation Interacted with: %@", NSStringFromCRToastInteractionType(interactionType));
+    }];
+}
+CRToastInteractionResponder * DMTapResponder() {
+    return [CRToastInteractionResponder interactionResponderWithInteractionType:CRToastInteractionTypeTap automaticallyDismiss:YES block:^(CRToastInteractionType interactionType) {
+        NSLog(@"Notifcation Interacted with: %@", NSStringFromCRToastInteractionType(interactionType));
+    }];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+NSDictionary * DMToastDefaultDictionary() {
+    return @{kCRToastTextAlignmentKey : @(NSTextAlignmentCenter),
+             kCRToastBackgroundColorKey: [UIColor colorWithRed:1 green:0.37 blue:0.0 alpha:1.0],
+             kCRToastAnimationInTypeKey : @(CRToastAnimationTypeGravity),
+             kCRToastAnimationOutTypeKey : @(CRToastAnimationTypeGravity),
+             kCRToastAnimationInDirectionKey : @(CRToastAnimationDirectionTop),
+             kCRToastAnimationOutDirectionKey : @(CRToastAnimationDirectionTop),
+             kCRToastNotificationPresentationTypeKey : @(CRToastPresentationTypePush),
+             kCRToastNotificationTypeKey : @(CRToastTypeNavigationBar),
+             kCRToastTimeIntervalKey : @(7)
+             };
+}
+
+@implementation ViewController
+
+- (IBAction)withTapPressed:(UIButton *)sender {
+    NSMutableDictionary *options = [DMToastDefaultDictionary() mutableCopy];
+    
+    options[kCRToastInteractionRespondersKey] = @[DMTapResponder()];
+    options[kCRToastTextKey] = @"Only Tap";
+    
+    [CRToastManager showNotificationWithOptions:options completionBlock:nil];
+}
+- (IBAction)withSwipePressed:(UIButton *)sender {
+    NSMutableDictionary *options = [DMToastDefaultDictionary() mutableCopy];
+    
+    options[kCRToastInteractionRespondersKey] = @[DMSwipeResponder()];
+    options[kCRToastTextKey] = @"Only Swipe";
+    
+    [CRToastManager showNotificationWithOptions:options completionBlock:nil];
+}
+- (IBAction)withTapAndSwipePressed:(UIButton *)sender {
+    NSMutableDictionary *options = [DMToastDefaultDictionary() mutableCopy];
+    
+    options[kCRToastInteractionRespondersKey] = @[DMTapResponder(), DMSwipeResponder()];
+    options[kCRToastTextKey] = @"Why not both?";
+    
+    [CRToastManager showNotificationWithOptions:options completionBlock:nil];
+}
+- (IBAction)withTapAndSwipeDifferentPressed:(UIButton *)sender {
+    NSMutableDictionary *options = [DMToastDefaultDictionary() mutableCopy];
+    
+    options[kCRToastInteractionRespondersKey] = @[DMSwipeResponder(), DMTapResponder()];
+    options[kCRToastTextKey] = @"Why not both?\nBut with a new order";
+    
+    [CRToastManager showNotificationWithOptions:options completionBlock:nil];
 }
 
 @end
