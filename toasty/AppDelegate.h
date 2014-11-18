8693ec8421f8ae7a87015ff161a62687c7257822
dmiedema <daniel@househappy.org>
2014-11-18 06:45:48 -0800

    Initial Commit

---
+++ toasty/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  toasty
+//
+//  Created by Daniel on 11/18/14.
+//  Copyright (c) 2014 Househappy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ toasty/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  toasty
+//
+//  Created by Daniel on 11/18/14.
+//  Copyright (c) 2014 Househappy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
